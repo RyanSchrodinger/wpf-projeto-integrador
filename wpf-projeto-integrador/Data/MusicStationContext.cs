e69d14bc1871d3352b2bdc02c615f5e4fa5d5e96
Ryan <ronaldsagan000@gmail.com>
2026-05-10 13:23:39 -0300

    feat: implementar modelagem tpt administradores

---
+++ wpf-projeto-integrador/Data/MusicStationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_projeto_integrador.Models;
+
+namespace wpf_projeto_integrador.Data
+{
+    public class MusicStationContext : DbContext 
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ wpf-projeto-integrador/Models/Administrador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_projeto_integrador.Models
+{
+    public class Administrador : Usuario
+    {
+        public NivelAcessoEnum NivelAcesso { get; set; }
+        public string? Observacao { get; set; }
+
+
+        public enum NivelAcessoEnum
+        {
+            Baixo,  
+            Medio,
+            Alto
+        }
+    }
+}
