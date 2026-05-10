6cb432410f664918da39ce96239a7b8036afb580
Ryan <ronaldsagan000@gmail.com>
2026-05-10 12:50:37 -0300

    Adicionar arquivos de projeto.

---
+++ wpf-projeto-integrador/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf_projeto_integrador
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
