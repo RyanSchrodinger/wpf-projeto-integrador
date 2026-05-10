55464f753d969cc97587edf1a7cd8ff85cfac4fd
Ryan <ronaldsagan000@gmail.com>
2026-05-10 13:00:00 -0300

    feat: implementar modelagem tpt para usuarios

---
+++ wpf-projeto-integrador/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_projeto_integrador.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SenhaHash { get; set; }
+        public string NomeUsuario  { get; set; }
+
+        public string? Email { get; set; } 
+        public DateTime DataCriacao { get; set; }
+
+
+    }
+}
