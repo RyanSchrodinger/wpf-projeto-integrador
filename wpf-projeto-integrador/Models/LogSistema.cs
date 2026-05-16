4ab2bcb83c3cf5aeb5ec6477f9887a109bbd8587
Ryan <ronaldsagan000@gmail.com>
2026-05-16 14:06:56 -0300

    feat: modelagem das classes log e tipoacao

---
+++ wpf-projeto-integrador/Models/LogSistema.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_projeto_integrador.Models
+{
+    public class LogSistema
+    {
+        public int Id { get; set; }
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public int TipoAcaoId { get; set; }
+        public TipoAcao TipoAcao { get; set; }
+
+        public string? Entidade { get; set; }
+        public int? EntidadeId { get; set; }
+
+
+        public string Descricao { get; set; }
+        public DateTime DataHora { get; set; }
+
+    }
+}
---
+++ wpf-projeto-integrador/Models/TipoAcao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_projeto_integrador.Models
+{
+    public class TipoAcao
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+
+    }
+}
