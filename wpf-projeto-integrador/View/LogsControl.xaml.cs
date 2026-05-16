0bf3f18796248a0fd7dfc586e70b1744b024be66
Ryan <ronaldsagan000@gmail.com>
2026-05-16 15:07:33 -0300

    style: tela de logs

---
+++ wpf-projeto-integrador/View/LogsControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Windows.Controls;
+
+using wpf_projeto_integrador.Data;
+
+namespace wpf_projeto_integrador.View
+{
+    /// <summary>
+    /// Interação lógica para LogsControl.xam
+    /// </summary>
+    public partial class LogsControl : UserControl
+    {
+        public LogsControl()
+        {
+            InitializeComponent();
+            CarregarLogs();
+        }
+
+        public void CarregarLogs()
+        {
+            using (var db = new MusicStationContext())
+            {
+                var logs = db.LogsSistema
+                    .Include(l => l.Usuario)
+                    .Include(l => l.TipoAcao)
+                    .OrderByDescending(l => l.DataHora)
+                    .ToList();
+
+                DataContext = new
+                {
+                    Logs = logs
+                };
+            }
+        }
+    }
+}
