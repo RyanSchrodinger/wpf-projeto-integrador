1763fcf93fa11fe4b8a4a2b29c01be71752ae536
ryan.rssantos <ryan.rssantos@OSA0716347W11-1>
2026-06-09 15:26:29 -0300

    comentarios

--- wpf-projeto-integrador/Helpers/GerenciadorLogs.cs
+++ wpf-projeto-integrador/Helpers/GerenciadorLogs.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.Pkcs;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Pkcs;
 using wpf_projeto_integrador.Data;
 using wpf_projeto_integrador.Models;
 
@@ -29,5 +30,23 @@ namespace wpf_projeto_integrador.Helpers
         }
 
 
+        //Importância do uso de procedures como segurança para métodos de deburação, pois aparentemente conseguimos ter acesso em tempo real o que está acontecendo dentro da memória
+        // Isso envolve testes, caixa preta, caixa branca
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(
+        //          "Server=tcp:musicstation-db1.database.windows.net,1433;" +
+        //            "Initial Catalog=MusicStation;" +
+        //            "User ID=ryan;" +
+        //            "Password=W@choswick01;" +
+        //            "Encrypt=True;" +
+        //            "TrustServerCertificate=False;",
+        //            options =>
+        //            {
+        //                options.EnableRetryOnFailure();
+        //            });
+        //}
     }
 }
